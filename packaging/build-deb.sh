84d04aef01cf0558fec2779800c0a414708ac72b
Shannon Smith <infiltratr@yandex.com>
2026-08-15 13:57:05 +1000

    Package standalone C test-media GUI

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -64,13 +64,14 @@ INSTALLED_SIZE=$(du -sk "$STAGE/root/usr" | awk '{print $1}')
     printf 'Architecture: %s\n' "$ARCH"
     printf 'Maintainer: Shannon Smith\n'
     printf 'X-Linux-Defragger-Build: %s\n' "$BUILD_FLAVOR"
-    printf 'Depends: python3, python3-gi, python3-cairo, gir1.2-gtk-3.0, policykit-1, udisks2, util-linux, libext2fs2, libsqlite3-0, libssl3t64\n'
+    printf 'Depends: python3, python3-gi, python3-cairo, gir1.2-gtk-3.0, libgtk-3-0t64, policykit-1, udisks2, util-linux, libext2fs2, libsqlite3-0, libssl3t64\n'
     printf 'Installed-Size: %s\n' "$INSTALLED_SIZE"
     printf 'Description: Safe direct filesystem analysis and canonical layout rewriting\n'
     printf ' Linux Defragger analyses filesystem allocation and safely rewrites\n'
     printf ' supported unmounted FAT, exFAT, NTFS, EXT2/3/4, XFS, Amiga OFS/FFS\n'
     printf ' and HFS+/HFSX filesystems. Btrfs, classic HFS, APFS, Minix, UFS and ZFS\n'
-    printf ' remain analysis-only.\n'
+    printf ' remain analysis-only. The package also includes the separate all-C GTK\n'
+    printf ' Linux Defragger Test Media program for building sacrificial field-test disks.\n'
     printf ' %s\n' "$FLAVOR_DESCRIPTION"
 } >"$STAGE/root/DEBIAN/control"
 
