0171368fa6db88590b6c888e9cf6506d1444754e
Shannon Smith <infiltratr@yandex.com>
2026-08-19 23:14:52 +1000

    Remove redundant root installer wrapper

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: GPL-3.0-or-later
-# Thin repository-root launcher for the project installer.
-set -eu
-ROOT=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-exec "$ROOT/defragger/install.sh" "$@"
