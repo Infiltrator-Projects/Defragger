0b7ac35dc22fc0390bade75e114d881eca2572e0
Shannon Smith <infiltratr@yandex.com>
2026-09-02 20:43:58 +1000

    Expose trusted directory tree creation

--- defragger/src/core/ld_path.h
+++ defragger/src/core/ld_path.h
@@ -10,6 +10,7 @@ bool ld_path_is_derived_from(const char *candidate, const char *base,
                              const char *suffix);
 FILE *ld_path_open_atomic_temp(const char *target_path, char **temporary_path);
 char *ld_path_parent_directory(const char *path);
+int ld_path_ensure_trusted_directory_tree(const char *path);
 void ld_path_fsync_parent(const char *path);
 
 #endif
