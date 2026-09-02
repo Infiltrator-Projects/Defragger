1b98180ab48509f55a49c500a7d69800d0cf7fcb
Shannon Smith <infiltratr@yandex.com>
2026-09-02 20:43:51 +1000

    Add journal-bound raw target open API

--- defragger/src/core/ld_device.h
+++ defragger/src/core/ld_device.h
@@ -14,10 +14,19 @@ typedef struct {
     bool is_block;
     uint64_t size_bytes;
     dev_t device_number;
+    dev_t host_device;
+    ino_t inode;
 } LdDevice;
 
 bool ld_device_number_is_mounted(dev_t device_number);
 bool ld_path_is_mounted(const char *path);
+int ld_device_try_open(const char *path, bool writable, LdDevice *device);
+bool ld_device_matches_identity(const LdDevice *device,
+                                const char *expected_identity,
+                                uint64_t expected_size);
+int ld_device_open_verified_fd(const char *path, bool writable,
+                               const char *expected_identity,
+                               uint64_t expected_size);
 LdDevice ld_device_open(const char *path, bool writable);
 void ld_device_close(LdDevice *device);
 bool ld_device_is_rotational(const LdDevice *device);
